5b8d84c2c4c0073cb5a940a9b4ef5dd1f32f8804
Shind <Shind@Anuja>
2023-07-09 19:42:25 +0530

    Second Commit

---
+++ MyProject/src/pack1/Calculator.java
@@ -0,0 +1,14 @@
+package pack1;
+
+public class Calculator {
+	int N1= 5, N2=2;
+	public void add()
+	{
+System.out.println("Sum is" + (N1+N2));
+	}
+public static void main(String []args)
+{
+Calculator cal= new Calculator();
+cal.add();
+}
+}
